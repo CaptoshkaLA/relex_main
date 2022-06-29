4378f102349b5193ffaec9ad9235c7a577dba225
CaptoshkaLA <captoshkala@gmail.com>
2022-06-29 12:11:17 +0300

    [19] Refactoring

--- index.js
+++ index.js
@@ -8,38 +8,38 @@ function main() {
     let player = new Player;
     let ai = new AI;
     let k = 0;
-    let fl;
+    let fl = {val: 0};
     game.gameStart();
     game.actionDiv.addEventListener("click", function() {
       if(k === 0) {
         k++;
-        fl = game.whoGoesFirst();
+        fl.val = game.whoGoesFirst();
         game.tableInitialize();
-        if(fl === 0) {
+        if(fl.val === 0) {
           let flag = 0;
           game = player.attack(game, flag);
-          fl = 1;
+          fl.val = 1;
         } else {
           game = ai.attack(game);
-          fl = 0;
+          fl.val = 0;
         }
       } else {
         while (true) {
-          alert(fl);
-          if (fl === 0) {
+          alert(fl.val);
+          if (fl.val === 0) {
             let flag = 0;
             alert("It's your turn now ")
             game.actionDiv.addEventListener("click", function() {
+              fl.val = 1;
               game = player.attack(game, flag);
             }, {once: true});
-            fl = 1;
-          } else if (fl === 1) {
+          } else if (fl.val === 1) {
             ai.attack(game);
             alert("It's computer turn now ")
             game.actionDiv.addEventListener("click", function() {
+              fl.val = 0;
               game = ai.attack(game);
             }, {once: true});
-            fl = 0;
           }
 
           if (game.defeat()) {

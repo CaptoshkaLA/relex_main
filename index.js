d7cdc1abc699bc3828eea5390e58caddb554cceb
CaptoshkaLA <captoshkala@gmail.com>
2022-06-29 03:47:08 +0300

    [13] Diversified the logic of artificial intelligence

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import {Game} from './src/game.js';
 import {Player} from "./src/player.js";
+import {AI} from './src/artifical_intelligence.js'
 
 function main() {
 
   let game = new Game;
   let player = new Player;
+  let ai = new AI;
   game.gameStart();
   game.actionDiv.addEventListener("click", function() {
     let fl = game.whoGoesFirst();
@@ -13,7 +15,7 @@ function main() {
       let flag = 0;
       player.attack(game, flag);
     } else {
-      //ai.attackAI();
+      ai.attack(game);
     }
   });
 }
--- src/artifical_intelligence.js
+++ src/artifical_intelligence.js
@@ -3,5 +3,29 @@ export class AI {
         let attCard = game.computerDeck.shift();
         game.attackCard.src="images/" + attCard[0] + "_of_" + attCard[1] + ".png";
         let rang = attCard[0];
+        let possibleCards = [];
+        for (let i = 2; i >= 0; i--) {
+            if(game.playerHand[i][0] < rang) {
+                possibleCards.push(i);
+            }
+        }
+        if(possibleCards.length === 0) {
+            game.playerDeck.push(game.attackCard);
+            alert("Computers card is added to your deck");
+        } else {
+            while(possibleCards.length != 0) {
+                let index = possibleCards.pop();
+                if(index === 2) {
+                    game.computerDeck.push(game.computerCard1);
+                    alert("Computer beat your first card and took it for itself")
+                } else if(index === 1) {
+                    game.computerDeck.push(game.computerCard2);
+                    alert("Computer beat your second card and took it for itself")
+                } else {
+                    game.computerDeck.push(game.computerCard3);
+                    alert("Computer beat your third card and took it for itself")
+                }
+            }
+        }
     }
 }
